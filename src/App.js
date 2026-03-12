040c80f6ed1bdc38e4659f9b11d5c168669ede44
OneaDarius <dariusonea320@gmail.com>
2026-03-12 14:18:02 +0200

    Am creat fereasta jocului, entitatea Mario și ciclul repetiv

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import EcranJoc from './componente/EcranJoc.jsx';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <EcranJoc></EcranJoc>
       </header>
     </div>
   );
--- src/componente/EcranJoc.jsx
+++ src/componente/EcranJoc.jsx
@@ -1,22 +1,41 @@
 import React, { useRef, useEffect } from 'react';
 import Mario from '../entitati/Mario.js';
 
+/**
+ *Componentă React responsabilă pentru afisarea suprafeței de joc
+ *Și gestionareaa buclei principale de animație
+ */ 
 const EcranJoc = () => {
+    //Referință către <canvas> (Element DOM)
     const canvasRef = useRef(null);
 
     useEffect(() => {
-        // TODO Student: 
-        // 1. Obține contextul 2D al canvas-ului.
-        // 2. Creează o instanță de Mario.
-        // 3. Implementează funcția loop() (Game Loop).
-        // 4. Pornește animația cu requestAnimationFrame.
+        const canvas = canvasRef.current;
+
+        if (!canvas) return;
+        
+        const context = canvas.getContext('2d');
+        const mario = new Mario(100, 100);
+        mario.viteza.seteaza(2, 0);
+        
+        /**
+         *Funcție recursivă pentru ciclul repetitiv care face funcțional jocul
+         *Și care execută împrospătare ecranului
+         */ 
+        const loop = () => {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            mario.actualizeaza();
+            mario.deseneaza(context);
+            requestAnimationFrame(loop);
+        };
+        requestAnimationFrame(loop);
     }, []);
 
     return (
-        <canvas 
-            ref={canvasRef} 
-            width={640} 
-            height={480} 
+        <canvas
+            ref={canvasRef}
+            width={640}
+            height={480}
             style={{ border: '2px solid black', background: '#5c94fc' }}
         />
     );
